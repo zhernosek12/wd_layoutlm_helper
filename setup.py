7fe66136543bf416835119e94d9aa33d48050789
zhernosek12 <zhernosek12@gmail.com>
2022-07-12 19:23:59 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from wdtools import __version__
+
+setup(name='wdtools',
+      version=__version__,
+      description='',
+      author='Zhernosek Andrei',
+      author_email='zhernosek12@gmail.com',
+      url='https://github.com/zhernosek12/wd_layoutlm_helper',
+      license='GPL',
+      packages=['wdtools']
+     )
